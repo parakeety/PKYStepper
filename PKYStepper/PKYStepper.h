f5eeb759b0797d8f008582bcc920dbf878354f81
yohei okada <yohei.states@akane.waseda.jp>
2015-01-11 22:58:18 -0800

    Rename directory from PKYStepper to PKYStepperExample

---
+++ PKYStepper/PKYStepper.h
@@ -0,0 +1,13 @@
+//
+//  PKYStepper.h
+//  PKYStepper
+//
+//  Created by Okada Yohei on 1/11/15.
+//  Copyright (c) 2015 yohei okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKYStepper : UIControl
+
+@end
---
+++ PKYStepper/PKYStepper.m
@@ -0,0 +1,21 @@
+//
+//  PKYStepper.m
+//  PKYStepper
+//
+//  Created by Okada Yohei on 1/11/15.
+//  Copyright (c) 2015 yohei okada. All rights reserved.
+//
+
+#import "PKYStepper.h"
+
+@implementation PKYStepper
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
