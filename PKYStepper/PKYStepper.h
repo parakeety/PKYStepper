c72a323f29c36415556bdf5a0bd6ccae075c5670
yohei okada <yohei.states@akane.waseda.jp>
2015-01-25 22:30:21 -0800

    Create prototype of PKYStepper

--- PKYStepper/PKYStepper.h
+++ PKYStepper/PKYStepper.h
@@ -8,6 +8,46 @@
 
 #import <UIKit/UIKit.h>
 
+@class PKYStepper;
+// called when value is changed
+typedef void (^PKYStepperValueChangedCallback)(PKYStepper *stepper, float newValue);
+
+// called when value is incremented
+typedef void (^PKYStepperIncrementedCallback)(PKYStepper *stepper, float newValue);
+
+
+// called when value is decremented
+typedef void (^PKYStepperDecrementedCallback)(PKYStepper *stepper, float newValue);
+
+IB_DESIGNABLE
 @interface PKYStepper : UIControl
+@property(nonatomic, strong) UILabel *countLabel;
+@property(nonatomic, strong) UIColor *labelColor;
+@property(nonatomic, strong) UIButton *incrementButton;
+@property(nonatomic, strong) UIButton *decrementButton;
+
+@property(nonatomic) float value; // default: 0.0
+@property(nonatomic) float stepInterval; // default: 1.0
+@property(nonatomic) float minimum; // default: 0.0
+@property(nonatomic) float maximum; // default: 100.0
+@property(nonatomic) BOOL hidesDecrementWhenMinimum; // default: NO
+@property(nonatomic) BOOL hidesIncrementWhenMaximum; // default: NO
+@property(nonatomic) CGFloat buttonWidth; // default: 44.0f
+
+@property(nonatomic, copy) PKYStepperValueChangedCallback valueChangedCallback;
+@property(nonatomic, copy) PKYStepperIncrementedCallback incrementCallback;
+@property(nonatomic, copy) PKYStepperDecrementedCallback decrementCallback;
+
+// call this method after setting value(s) and callback(s)
+// This method will call callback
+- (void)setup;
+
+// view customization
+- (void)setBorderColor:(UIColor *)color;
+- (void)setTextColor:(UIColor *)color;
+- (void)setBorderWidth:(CGFloat)width;
+- (void)setCornerRadius:(CGFloat)radius;
+- (void)setLabelFont:(UIFont *)font;
+- (void)setButtonFont:(UIFont *)font;
 
 @end
--- PKYStepper/PKYStepper.m
+++ PKYStepper/PKYStepper.m
@@ -6,16 +6,204 @@
 //  Copyright (c) 2015 yohei okada. All rights reserved.
 //
 
+// action control: UIControlEventApplicationReserved for increment/decrement?
+// delegate: if there are multiple PKYSteppers in one viewcontroller, it will be a hassle to identify each PKYSteppers
+// block: watch out for retain cycle
+
+// check visibility of buttons when
+// 1. right before displaying for the first time
+// 2. value changed
+
 #import "PKYStepper.h"
+
+static const float kButtonWidth = 44.0f;
 
 @implementation PKYStepper
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark initialization
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame])
+    {
+        [self commonInit];
+    }
+    return self;
 }
-*/
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder])
+    {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit
+{
+    _value = 0.0f;
+    _stepInterval = 1.0f;
+    _minimum = 0.0f;
+    _maximum = 100.0f;
+    _hidesDecrementWhenMinimum = NO;
+    _hidesIncrementWhenMaximum = NO;
+    _buttonWidth = kButtonWidth;
+    
+    self.clipsToBounds = YES;
+    [self setBorderWidth:1.0f];
+    [self setCornerRadius:3.0];
+    
+    self.countLabel = [[UILabel alloc] init];
+    self.countLabel.textAlignment = NSTextAlignmentCenter;
+    self.countLabel.layer.borderWidth = 1.0f;
+    [self addSubview:self.countLabel];
+    
+    self.incrementButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.incrementButton setTitle:@"+" forState:UIControlStateNormal];
+    [self.incrementButton addTarget:self action:@selector(incrementButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.incrementButton];
+    
+    self.decrementButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.decrementButton setTitle:@"-" forState:UIControlStateNormal];
+    [self.decrementButton addTarget:self action:@selector(decrementButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.decrementButton];
+    
+    UIColor *defaultColor = [UIColor colorWithRed:(79/255.0) green:(161/255.0) blue:(210/255.0) alpha:1.0];
+    [self setBorderColor:defaultColor];
+    [self setTextColor:defaultColor];
+    
+    [self setLabelFont:[UIFont fontWithName:@"Avernir-Roman" size:14.0f]];
+    [self setButtonFont:[UIFont fontWithName:@"Avenir-Black" size:24.0f]];
+}
+
+
+#pragma mark render
+- (void)layoutSubviews
+{
+    CGFloat width = self.bounds.size.width;
+    CGFloat height = self.bounds.size.height;
+    
+    self.countLabel.frame = CGRectMake(self.buttonWidth, 0, width - (self.buttonWidth * 2), height);
+    self.incrementButton.frame = CGRectMake(width - self.buttonWidth, 0, self.buttonWidth, height);
+    self.decrementButton.frame = CGRectMake(0, 0, self.buttonWidth, height);
+    
+    self.incrementButton.hidden = (self.hidesIncrementWhenMaximum && [self isMaximum]);
+    self.decrementButton.hidden = (self.hidesDecrementWhenMinimum && [self isMinimum]);
+}
+
+- (void)setup
+{
+    if (self.valueChangedCallback)
+    {
+        self.valueChangedCallback(self, self.value);
+    }
+}
+
+- (CGSize)sizeThatFits:(CGSize)size
+{
+    if (CGSizeEqualToSize(size, CGSizeZero))
+    {
+        // if CGSizeZero, return ideal size
+        CGSize labelSize = [self.countLabel sizeThatFits:size];
+        return CGSizeMake(labelSize.width + (self.buttonWidth * 2), labelSize.height);
+    }
+    return size;
+}
+
+
+#pragma mark view customization
+- (void)setBorderColor:(UIColor *)color
+{
+    self.layer.borderColor = color.CGColor;
+    self.countLabel.layer.borderColor = color.CGColor;
+}
+
+- (void)setTextColor:(UIColor *)color
+{
+    self.countLabel.textColor = color;
+    [self.incrementButton setTitleColor:color forState:UIControlStateNormal];
+    [self.decrementButton setTitleColor:color forState:UIControlStateNormal];
+}
+
+- (void)setBorderWidth:(CGFloat)width
+{
+    self.layer.borderWidth = width;
+}
+
+- (void)setCornerRadius:(CGFloat)radius
+{
+    self.layer.cornerRadius = radius;
+}
+
+- (void)setLabelFont:(UIFont *)font
+{
+    self.countLabel.font = font;
+}
+
+- (void)setButtonFont:(UIFont *)font
+{
+    self.incrementButton.titleLabel.font = font;
+    self.decrementButton.titleLabel.font = font;
+}
+
+
+#pragma mark setter
+- (void)setValue:(float)value
+{
+    _value = value;
+    if (self.hidesDecrementWhenMinimum)
+    {
+        self.decrementButton.hidden = [self isMinimum];
+    }
+    
+    if (self.hidesIncrementWhenMaximum)
+    {
+        self.incrementButton.hidden = [self isMaximum];
+    }
+    
+    if (self.valueChangedCallback)
+    {
+        self.valueChangedCallback(self, _value);
+    }
+}
+
+
+
+#pragma mark event handler
+- (void)incrementButtonTapped:(id)sender
+{
+    if (self.value < self.maximum)
+    {
+        self.value += self.stepInterval;
+        if (self.incrementCallback)
+        {
+            self.incrementCallback(self, self.value);
+        }
+    }
+}
+
+- (void)decrementButtonTapped:(id)sender
+{
+    if (self.value > self.minimum)
+    {
+        self.value -= self.stepInterval;
+        if (self.decrementCallback)
+        {
+            self.decrementCallback(self, self.value);
+        }
+    }
+}
+
+
+#pragma mark private helpers
+- (BOOL)isMinimum
+{
+    return self.value == self.minimum;
+}
+
+- (BOOL)isMaximum
+{
+    return self.value == self.maximum;
+}
 
 @end
