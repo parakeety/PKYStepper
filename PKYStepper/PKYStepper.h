92d9fd3d2d3890bb2448c34a459bcc1898e0b944
soulshined <soulshined@users.noreply.github.com>
2015-09-20 18:06:58 -0400

    Update PKYStepper.h
    Created variables for complete Interface Builder aggregation. Created IBInspectable tags for the stepper's respective properties. Now users can 100% use storyboard's attribute tab, they no longer need to set anything in their View Controllers implementation outside of callbacks.

--- PKYStepper/PKYStepper.h
+++ PKYStepper/PKYStepper.h
@@ -20,18 +20,24 @@ typedef void (^PKYStepperIncrementedCallback)(PKYStepper *stepper, float newValu
 typedef void (^PKYStepperDecrementedCallback)(PKYStepper *stepper, float newValue);
 
 IB_DESIGNABLE
-@interface PKYStepper : UIControl
+@interface PKYStepper : UIControl {
+    IBInspectable UIColor *borderColor; //Default lightBlue
+    IBInspectable float borderWidth; //Default 1.0
+    IBInspectable UIColor *labelTextColor; //Default lightBlue
+    IBInspectable UIColor *labelColor; //Default whiteColor
+    IBInspectable UIColor *buttonTextColor; //Default lightBlue
+    IBInspectable UIColor *buttonBackgroundColor; //Default whiteColor
+}
 @property(nonatomic, strong) UILabel *countLabel;
-@property(nonatomic, strong) UIColor *labelColor;
 @property(nonatomic, strong) UIButton *incrementButton;
 @property(nonatomic, strong) UIButton *decrementButton;
 
-@property(nonatomic) float value; // default: 0.0
-@property(nonatomic) float stepInterval; // default: 1.0
-@property(nonatomic) float minimum; // default: 0.0
-@property(nonatomic) float maximum; // default: 100.0
-@property(nonatomic) BOOL hidesDecrementWhenMinimum; // default: NO
-@property(nonatomic) BOOL hidesIncrementWhenMaximum; // default: NO
+@property(nonatomic) IBInspectable float value; // default: 0.0
+@property(nonatomic) IBInspectable float stepInterval; // default: 1.0
+@property(nonatomic) IBInspectable float minimumValue; // default: 0.0
+@property(nonatomic) IBInspectable float maximumValue; // default: 100.0
+@property(nonatomic) IBInspectable BOOL hidesDecrementWhenMinimum; // default: NO
+@property(nonatomic) IBInspectable BOOL hidesIncrementWhenMaximum; // default: NO
 @property(nonatomic) CGFloat buttonWidth; // default: 44.0f
 
 @property(nonatomic, copy) PKYStepperValueChangedCallback valueChangedCallback;
